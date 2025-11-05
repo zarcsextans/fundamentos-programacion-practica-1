f3c9307257057ce000ee0a1aacd40b1f94924863
Scarlet Ruelas <Zarcsextans@gmail.com>
2025-11-05 13:12:38 -0600

    ejercicio 2 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -31,10 +31,10 @@ function miInformacion() {
 function operacionesBasicas(a, b) {
   // TODO: Calcula suma, resta, multiplicaciÃ³n y divisiÃ³n de a y b
   
-  const suma = 0;
-  const resta = 0;
-  const multiplicacion = 0;
-  const division = 0;
+  const suma = a+b;
+  const resta = a-b;
+  const multiplicacion = a*b;
+  const division = a/b;
   
   return { suma, resta, multiplicacion, division };
 }
