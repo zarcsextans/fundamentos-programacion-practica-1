023226664c3e522f06ddb2d9bea9671f30590976
Scarlet Ruelas <Zarcsextans@gmail.com>
2025-11-05 13:30:51 -0600

    ejercicio 4 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -58,9 +58,9 @@ function areaRectangulo(base, altura) {
  * @param {number} celsius - Temperatura en Celsius
  * @returns {number} - Temperatura en Fahrenheit
  */
+return F= Celsius * 9/5 + 32;
 function celsiusAFahrenheit(celsius) {
   // TODO: Implementa la conversiÃ³n
-  return Fahrenheit= Celsius * 9/5 + 32;
 }
 
 // ============================================
