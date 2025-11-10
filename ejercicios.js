e43d5b926f825b4b9275727f93debe977e1287bb
ScarletRuelas <Zarcsextans@gmail.com>
2025-11-10 09:15:36 -0600

    ejercicio 7

--- ejercicios.js
+++ ejercicios.js
@@ -108,6 +108,13 @@ function evaluarNota(nota) {
  */
 function mayorDeTres(a, b, c) {
   // TODO: Encuentra y retorna el mayor de los tres nÃºmeros
+  if (a >= b && a >= c) {
+    return a;
+  } else if (b >= a && b >= c) {
+    return b;
+  } else {
+    return c;
+  }
   return 0;
 }
 
