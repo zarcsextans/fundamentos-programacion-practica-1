1dfd96e58cf7eeef1b403675c24dafa8b55fbd9d
ScarletRuelas <Zarcsextans@gmail.com>
2025-11-11 22:09:14 -0600

    Ejercicio 13

--- ejercicios.js
+++ ejercicios.js
@@ -222,8 +222,13 @@ const pares = [];
  * @returns {number} - Suma de todos los elementos
  */
 function sumaArray(numeros) {
-  // TODO: Suma todos los elementos del array
-  return 0;
+  let suma = 0;
+
+  for (let i = 0; i < numeros.length; i++) {
+    suma += numeros[i];
+  }
+
+  return suma;
 }
 
 /**
